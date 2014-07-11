565a7d5bae18e6dec456d43605eb9cd0fca692ab
Sam Lopes <sam@thinkingbig.net>
2014-07-11 14:49:08 -0300

    Added render and background.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-var WORLDCUPSOCCER = WORLDCUPSOCCER || {};
+var canvas = document.getElementById("cvs"),
+	context = canvas.getContext("2d");
+canvas.width = 150,
+canvas.height = 150;
 
-WORLDCUPSOCCER = {
-	canvas: document.getElementById("cvs"),
-	context: canvas.getContext("2d")
 
-	keyIsPressed: function () {
-		console.log("A key has been pressed");
-	};
+
+function render () {
+	context.beginPath();
+	context.rect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = "#000000";
+	context.fill();
+	context.closePath();
 }
 
-document.addEventListener("keydown", WORLDCUPSOCCER.keyIsPressed, false);
+function keyPressed () {
+	console.log("A key has been pressed");
+};
+
+document.addEventListener("keydown", keyPressed, false);
+
+render();
