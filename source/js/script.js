8423b2e1dec08a43d6d9200fce6ee80445aae078
Sam Lopes <sam@thinkingbig.net>
2014-07-15 14:09:05 -0300

    added PILLS

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,11 @@ var canvas = document.getElementById("cvs"),
 	fps = 10,
 	drawInterval,
 	playersImg = new Image(),
-	keysDown = {};
+	keysDown = {},
+	randPlaceX,
+	randPlaceY,
+	randSizeX,
+	randSizeY;
 
 // Sets the path of the spritesheet
 playersImg.src = "source/img/players.png";
@@ -21,6 +25,15 @@ context.imageSmoothingEnabled = false;
 
 // The GAME namespace
 GAME = {
+	pills: {
+		x: (canvas.width / 2) - 100,
+		y: (canvas.height / 2) - 100,
+		Y: 27,
+		X: 0,
+		sizeX: 48,
+		sizeY: 24,
+		count: 0
+	},
 
 	// The player's properties
 	player: {
@@ -28,6 +41,8 @@ GAME = {
 		y: (canvas.height /2) - 16,
 		X: 12,
 		Y: 8,
+		sizeX: 24,
+		sizeY: 32,
 		speed: 100
 	},
 
@@ -45,24 +60,32 @@ GAME = {
 	width: canvas.width,
 	height: canvas.height,
 	functions: {
+		// Alerts you if you are getting too big
+		alertSize: function () {
+			if (GAME.pills.count > 2900) {
+				context.font = "48px soccer";
+				context.fillStyle = "#000000";
+				context.fillText("You broke it...", (canvas.width / 2) - 160, canvas.height / 2);
+			}
+		},
 
 		// Checks if the player touches the border
 		checkBorderCollisions: function () {
-			if (GAME.player.x > (GAME.width + 24)) {
-				GAME.player.x -= 24;
-			} else if (GAME.player.x > (GAME.width - 24)) {
+			if (GAME.player.x > (GAME.width + GAME.player.sizeX)) {
+				GAME.player.x -= GAME.player.sizeX;
+			} else if (GAME.player.x > (GAME.width - GAME.player.sizeX)) {
 				GAME.player.x -= 1;
-			} else if (GAME.player.x < (-48)) {
-				GAME.player.x += 24;
+			} else if (GAME.player.x < -(GAME.player.sizeX * 2)) {
+				GAME.player.x += GAME.player.sizeX;
 			} else if (GAME.player.x < 0) {
 				GAME.player.x += 1;
-			} else if (GAME.player.y < (-64)) {
-				GAME.player.y += 32;
+			} else if (GAME.player.y < -(GAME.player.sizeY * 2)) {
+				GAME.player.y += GAME.player.sizeY;
 			} else if (GAME.player.y < 0) {
 				GAME.player.y += 1;
-			} else if (GAME.player.y > (GAME.height + 32)) {
-				GAME.player.y -= 32;
-			} else if (GAME.player.y > (GAME.height - 32)) {
+			} else if (GAME.player.y > (GAME.height + GAME.player.sizeY)) {
+				GAME.player.y -= GAME.player.sizeY;
+			} else if (GAME.player.y > (GAME.height - GAME.player.sizeY)) {
 				GAME.player.y -= 1;
 			}
 		},
@@ -81,6 +104,29 @@ GAME = {
 				GAME.player.y = GAME.player.y - 400 / GAME.player.speed;
 			} else {
 				GAME.player.X = 12;
+			}
+			return false;
+		},
+
+		// Checks to see if playeer touches the pills
+		checkPillCollisions: function () {
+			if ((GAME.player.y + GAME.player.sizeY) > GAME.pills.y && GAME.player.y < (GAME.pills.y + 24) && (GAME.player.x + GAME.player.sizeX) > GAME.pills.x && GAME.player.x < (GAME.pills.x + 48)) {
+				GAME.player.sizeX *= 1.03;
+				GAME.player.sizeY *= 1.03;
+				GAME.player.speed *= 1.02;
+				flag = true;
+				while (flag === true) {
+					console.log("pill teleporting");
+					randPlaceX = (Math.floor(Math.random() * 1000));
+					randPlaceY = (Math.floor(Math.random() * 500));
+					if (randPlaceX < 742 && randPlaceX > 10 && randPlaceY > 10 && randPlaceY < 376) {
+						GAME.pills.x = randPlaceX;
+						GAME.pills.y = randPlaceY;
+						GAME.pills.count += 1;
+						flag = false;
+					}
+				}
+				console.log("pill teleported");
 			}
 		},
 
@@ -108,8 +154,12 @@ GAME = {
 			GAME.functions.checkKeys();
 			GAME.functions.checkRadioBtnsTicked();
 			GAME.functions.drawField(GAME.width, GAME.height);
-			GAME.functions.drawPlayer(GAME.player.X, GAME.player.Y, GAME.player.x, GAME.player.y);
+			GAME.functions.drawPill(GAME.pills.X, GAME.pills.Y, GAME.pills.x, GAME.pills.y, GAME.pills.sizeX, GAME.pills.sizeY);
+			GAME.functions.drawPlayer(GAME.player.X, GAME.player.Y, GAME.player.x, GAME.player.y, GAME.player.sizeX, GAME.player.sizeY);
 			GAME.functions.checkBorderCollisions();
+			GAME.functions.checkPillCollisions();
+			GAME.functions.drawText();
+			GAME.functions.alertSize();
 		},
 
 		// Draws the soccer field
@@ -138,7 +188,7 @@ GAME = {
 			context.lineTo(0, 300);
 			context.moveTo(x / 2, 0);
 			context.lineTo(x / 2, y);
-
+			
 			// How all the lines and circles are styled
 			context.lineWidth = 5;
 			context.strokeStyle = "#ffffff";
@@ -147,8 +197,19 @@ GAME = {
 		},
 
 		// Draws the player
-		drawPlayer: function (srcX, srcY, posX, posY) {
-			context.drawImage(playersImg, srcX, srcY, 6, 8, posX, posY, 24, 32);
+		drawPlayer: function (srcX, srcY, posX, posY, sizeX, sizeY) {
+			context.drawImage(playersImg, srcX, srcY, 6, 8, posX, posY, sizeX, sizeY);
+		},
+
+		drawPill: function (srcX, srcY, posX, posY, sizeX, sizeY) {
+			context.drawImage(playersImg, srcX, srcY, 16, 8, posX, posY, sizeX, sizeY);
+		},
+
+		// Draws text
+		drawText: function () {
+			context.font = "16px soccer";
+			context.fillStyle = "#ffffff"
+			context.fillText("You found " + GAME.pills.count + " pills and ate them.", 5, 15);
 		},
 
 		// Begins the loop
